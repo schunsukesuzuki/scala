2969198980683dacdc785475c6ada8ac2c8b3b56
schunsukesuzuki <schunsuke.suzuki@gmail.com>
2018-02-28 12:14:02 +0900

    recommit

--- n.scala
+++ n.scala
@@ -1,16 +1,14 @@
+object Main extends App{
 import scala.io.StdIn.readLine
-
-object n {
-  def main(args: Array[String]): Unit = {
-    marking
+  override def main(args: Array[String]): Unit = {
+      marking
   }
   /* print (" plz enter number ")  */
   var i = readInt()
-  
   def marking = {
-    if(0 < i && i < 100) { 
+    if(0 < i && i < 100) {
       for ( j <- 1 to i ) {
-      print( " * " )
+      print( "*" )
       }
     }
   }
