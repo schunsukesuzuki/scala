522cedc55cde60b7f2f3aa1e0f52e697c8776297
schunsukesuzuki <schunsuke.suzuki@gmail.com>
2018-03-15 12:13:04 +0900

    recommit

---
+++ orderedMergeSort.scala
@@ -0,0 +1,22 @@
+class Queue[+T] private (
+  private [this] var leading: List[T],
+  private [this] var trailing: List[T]
+) {
+  private def mirror() =
+    if (leading.is Empty) {
+      while (!trailing.is Empty) {
+        leading = trailing.head :: leading
+        trailing = trailing.tail
+      }
+    }
+  def head: T = {
+    mirror()
+    leading.head
+  }
+  def tail: Queue[T] = {
+    mirror()
+    new Queue(leading.tail, trailing)
+  }
+  def enqueue[U >: T](x: U) =
+    new Queue[U] (leading,x :: trailing)
+}
