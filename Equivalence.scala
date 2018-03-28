3e1431259e4de809df9f1f48114ba839968a7559
schunsukesuzuki <schunsuke.suzuki@gmail.com>
2018-03-28 21:44:00 +0900

    recommit

---
+++ Equivalence.scala
@@ -0,0 +1,43 @@
+//https://qiita.com/f81@github/items/a90c16419645be5d7008
+
+object Equivalence {
+
+  def main(args: Array[String]): Unit = {
+    val domain1 = new Domain1(1, "富樫")
+    val domain2 = new Domain1(1, "富樫")
+
+    println(domain1 == domain2)
+    println(domain1 != domain2)
+
+    val domain3 = new Domain1(2, "虎丸")
+
+    println("==:", domain1 == domain3)
+    println("!=:", domain1 != domain3)
+  }
+
+}
+
+class Domain1(val id: Long, val name: String) {
+  def canEqual(other: Any) = {
+    other.isInstanceOf[Domain1]
+  }
+
+  override def equals(other: Any) = {
+    other match {
+      case that: Domain1 =>
+        that.canEqual(Domain1.this) && id == that.id && name == that.name
+      case _ => false
+    }
+  }
+
+  override def hashCode() = {
+    val prime = 41
+    prime * (prime + id.hashCode) + name.hashCode
+  }
+}
+
+//$ scala Equivalence
+//true   // domain1 == domain2
+//false  // domain1 != domain2
+//false  // domain1 == domain3
+//true   // domain1 != domain3
