e9f1989145b6e0fe4b6b93ef892976a999afa8a5
schunsukesuzuki <schunsuke.suzuki@gmail.com>
2018-03-30 13:26:07 +0900

    recommit

--- bfs.scala
+++ bfs.scala
@@ -1,23 +1,13 @@
 //https://github.com/vkostyukov/scalacaster/blob/master/src/graph/Graph.scala
 
-  def nodesByBreadth: List[N] = graphsByBreadth.map(_.value) 
-
   /**
-   * Returns all graphs that are connected to this graph arranged by DFS algorithm.
+   * Returns all nodes of this graph arranged by BFS algorithm.
    *
    * Time - O()
    * Space - O()
    */
-  
-  def graphsByDepth: List[Graph[E, N]] = { 
-    def loop(g: Graph[E, N], s: Set[Graph[E, N]]): Set[Graph[E, N]] =
-      if (!s(g)) {
-        val ss = g.succs.foleLeft(s + g)((acc, gg) => loop(gg, acc))
-        g.preds.foldLeft(ss)((acc,gg)) => loop(gg, acc))
-      } else s
 
-    loop(this, Set()).toList
-  }
+  def nodesByBreadth: List[N] = graphsByBreadth.map(_.value)
 
   /**
    * Returns all graphs that are connected to this graph arranged by BFS algorithm.
